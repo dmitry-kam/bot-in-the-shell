d2896dee60a0469e3a2e40b2ffe36865a87e73e0
d <dmitriys.kam@gmail.com>
2023-01-05 21:44:18 +0300

    temp

--- src/socketSniffer.py
+++ src/socketSniffer.py
@@ -14,6 +14,23 @@ def ws_trades():
     def on_error(wsapp, error):
         print(error)
 
+    # def on_open(ws):
+    #     print('opened connection')
+    #
+    # def on_close(ws):
+    #     print('close connection')
+    #
+    # def on_message(ws, message):
+    #     print('received message')
+    #     print(message)
+    #
+    # def on_error(ws, message):
+    #     print('error:', message)
+    #
+    # ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message,
+    #                             on_error=on_error)
+    # ws.run_forever()
+
     wsapp = websocket.WebSocketApp(socket, on_message=on_message, on_error=on_error)
     wsapp.run_forever()
 
@@ -31,3 +48,39 @@ def handle_trades(json_message):
     print("-----------------------")
 
 #ws_trades()
+#
+# """Демонстрация работы получения цен пары ETH-USDT по сокету"""
+#
+# def wsTradesNew():
+#     """Подключение"""
+#     socket = f'wss://ws-api.binance.com:443/ws-api/v3'
+#
+#     def on_message(wsapp, message):
+#         json_message = json.loads(message)
+#         print(json_message)
+#         #handle_trades(json_message)
+#
+#     def on_error(wsapp, error):
+#         print(error)
+#
+#     request =  '''
+#         {
+#           "id": "e2a85d9f-07a5-4f94-8d5f-789dc3deb097",
+#           "method": "order.place",
+#           "params": {
+#             "symbol": "BTCUSDT",
+#             "side": "BUY",
+#             "type": "LIMIT",
+#             "price": "0.1",
+#             "quantity": "10",
+#             "timeInForce": "GTC",
+#             "timestamp": 1655716096498,
+#             "apiKey": "T59MTDLWlpRW16JVeZ2Nju5A5C98WkMm8CSzWC4oqynUlTm1zXOxyauT8LmwXEv9",
+#             "signature": "5942ad337e6779f2f4c62cd1c26dba71c91514400a24990a3e7f5edec9323f90"
+#           }
+#         }
+#         '''
+#     wsapp = websocket.WebSocketApp(socket, header= request, on_message=on_message, on_error=on_error)
+#     wsapp.run_forever()
+#
+# wsTradesNew()
--- start.sh
+++ start.sh
@@ -7,3 +7,7 @@ rm get-pip.py
 pipenv shell
 pipenv update
 apt install mariadb-server
+
+################
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.5.3-amd64.deb
