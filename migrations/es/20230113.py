fc32ce50f4bb9a06956cc531b62925833feea71b
d <dmitriys.kam@gmail.com>
2023-02-08 18:09:44 +0300

    orjson (do: pipenv install)

--- migrations/es/20230113.py
+++ migrations/es/20230113.py
@@ -1,5 +1,6 @@
 import os
 import time
+import orjson
 import json
 import datetime
 import requests
@@ -75,9 +76,9 @@ def parseData(esConnection):
                     file.close()
             else:
                 url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/historical?id="+str(coin)+"&convertId=2781&timeStart="+str(startDate)+"&timeEnd="+str(endDate)
-                content = json.loads(requests.get(url).content)
+                content = orjson.loads(requests.get(url).content)
                 with open(os.path.dirname(os.path.realpath(__file__)) + TMP_FILES_DIRECTORY + str(coin) + "_" + str(year) + '.json', 'w') as file:
-                    file.write(json.dumps(content))
+                    file.write(orjson.dumps(content))
                     file.close()
 
             if (content['status']['error_message'] == "SUCCESS"):
